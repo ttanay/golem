e94cd288cf0185d44c901691ffa5c0da824d04c1
maaktweluit <10008353+maaktweluit@users.noreply.github.com>
2019-01-15 12:13:25 +0100

    Add sleep to catch exit code

--- scripts/test-daemon-start.ps1
+++ scripts/test-daemon-start.ps1
@@ -5,9 +5,15 @@ $HyperdriveProcess = Get-Process hyperg -ErrorAction SilentlyContinue
 if ($HyperdriveProcess) {
   Stop-Process -Id $HyperdriveProcess.Id
 }
+"Test..."
+$env:path
 
 "Starting..."
+where.exe hyperg
+
 $HyperdriveProcess = Start-Process C:\BuildResources\hyperg\hyperg.exe -WorkingDirectory C:\Users\buildbot-worker -PassThru
+
+Start-Sleep -s 5
 
 $HyperdriveProcess
 
