474cc36d6f8e08d019055d263842796ab738b77a
Kamil Śliwak <kamil.sliwak@codepoets.it>
2019-04-10 08:19:59 +0200

    get_metadata(): Clean up and slightly refactor the implementation

--- apps/transcoding/ffmpeg/utils.py
+++ apps/transcoding/ffmpeg/utils.py
@@ -172,41 +172,35 @@ class StreamOperator:
                                                     resources=resources,
                                                     logs=logs, work=work)
 
-    def get_metadata(self, basename_file_list: List[str], task_dir):
+    def get_metadata(self, input_files: List[str], task_dir):
+        try:
+            resources_dir = task_dir
+            output_dir = os.path.join(task_dir, 'metadata_output')
+            work_dir = os.path.join(task_dir, 'metadata_work')
+            os.makedirs(output_dir)
+            os.makedirs(work_dir)
+        except OSError:
+            raise ffmpegException(
+                "Failed to prepare directory structure for get_metadata")
 
-        def _prepare_dir_mapping():
-            try:
-                _resources_dir = task_dir
-                _output_dir = os.path.join(task_dir, 'metadata_output')
-                _work_dir = os.path.join(task_dir, 'metadata_work')
-                os.makedirs(_output_dir)
-                os.makedirs(_work_dir)
-            except OSError:
-                raise ffmpegException("Failed to prepare video \
-                           directory structure")
-            return _resources_dir, _work_dir, _output_dir
-
-        res_dir, work_dir, output_dir = _prepare_dir_mapping()
-
-        metadata_requests = []
-
-        for name in basename_file_list:
-            metadata_requests.append(
-                {'video': name,
-                 'output': 'metadata-logs-'+os.path.splitext(name)[0]+'.json'}
-            )
+        metadata_requests = [{
+            'video': input_file,
+            'output': f'metadata-logs-{os.path.splitext(input_file)[0]}.json'
+        } for input_file in input_files]
 
         extra_data = {
             'script_filepath': FFMPEG_BASE_SCRIPT,
             'command': Commands.COMPUTE_METRICS.value[0],
-            'metrics_params': {'metadata': metadata_requests},
+            'metrics_params': {
+                'metadata': metadata_requests,
+            },
         }
 
-        dir_mapping = DockerTaskThread.specify_dir_mapping(output=output_dir,
-                                                           temporary=work_dir,
-                                                           resources=res_dir,
-                                                           logs=work_dir,
-                                                           work=work_dir)
-        result = self._do_job_in_container(dir_mapping, extra_data)
+        dir_mapping = DockerTaskThread.specify_dir_mapping(
+            output=output_dir,
+            temporary=work_dir,
+            resources=resources_dir,
+            logs=work_dir,
+            work=work_dir)
 
-        return result
+        return self._do_job_in_container(dir_mapping, extra_data)
