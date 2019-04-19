cd664549f02a9ce236951cdd677d1a7e16e419be
pawel.kisielewicz@codepoets.it <pawel.kisielewicz@codepoets.it>
2019-04-19 13:48:28 +0200

    !fixup [TMP] Fix pylint and mypy warnings in base task implementation

--- apps/transcoding/task.py
+++ apps/transcoding/task.py
@@ -193,7 +193,6 @@ class TranscodingTaskBuilder(CoreTaskBuilder):
     SUPPORTED_FILE_TYPES: List[Container] = []
     SUPPORTED_VIDEO_CODECS: List[VideoCodec] = []
     SUPPORTED_AUDIO_CODECS: List[AudioCodec] = []
-    TASK_CLASS: CoreTask = TranscodingTask
 
     @classmethod
     def build_full_definition(cls, task_type: CoreTaskTypeInfo,
