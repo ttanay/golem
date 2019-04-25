a551d0b4918dac6c869e94a7e8505dc67c317985
Jakub Konka <jakub.konka@golem.network>
2019-04-25 14:32:41 +0200

    Add WASI app

---
+++ apps/wasi/resources/images/scripts/job.py
@@ -0,0 +1,35 @@
+import json
+import os
+import subprocess
+import shutil
+
+
+WASI_SANDBOX_EXECUTABLE_NAME = '/wasmtime'
+
+
+def run_job():
+    with open('params.json', 'r') as params_file:
+        params = json.load(params_file)
+
+    input_dir = os.path.join(
+        os.environ['RESOURCES_DIR'], params['workdir'])
+    output_dir = os.path.join(
+        os.environ['OUTPUT_DIR'], params['workdir'], params['name'])
+
+    shutil.copytree(os.path.join(input_dir, params['name']), output_dir)
+
+    subprocess.call(
+        [
+            WASI_SANDBOX_EXECUTABLE_NAME,
+            '--mapdir=.:' + output_dir,
+            os.path.join(input_dir,
+                         os.path.basename(params['bin'])),
+        ] + [
+            '--',
+        ] + params['exec_args'],
+        cwd=os.environ['RESOURCES_DIR']
+    )
+
+
+if __name__ == '__main__':
+    run_job()
